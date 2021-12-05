9dc6c0c9fd6d8338d10ddf428f73c8a7cdbf3b40
WePlayFleX <alex.kamai@ya.ru>
2021-12-05 22:56:00 +0300

    Добавьте файлы проекта.

---
+++ SQRT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double number, answer;
+	cout << "Enter number: ";
+	cin >> number;
+	answer = sqrt(number);
+	cout << "The square root is " << answer << endl;
+
+	system("pause");
+	return 0;
+}
